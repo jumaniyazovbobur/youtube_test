0c1a417ddce2c56ce5e6111bd3499dea657832e0
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2023-08-11 19:34:22 +0300

    University and Dean added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Assalomu aleykum!");
         System.out.println("MAZGI ACADEMY!");
+        System.out.println("Test-1");
     }
 }
